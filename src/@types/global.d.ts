7ae3d244f051f49813a4de2fca0f47e5a0299d3c
noahehall <noahehall@users.noreply.github.com>
2022-05-25 12:00:44 -0700

    upgrade pkgs and continue working through responsive-loader stuff

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -2,3 +2,29 @@ declare module '*.png' {
   const value: any;
   export = value;
 }
+
+declare module '*.jpg' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.webp' {
+  const value: any;
+  export = value;
+}
+
+// @see https://stackoverflow.com/questions/60816666/how-to-use-query-param-import-in-webpack-with-typescript-without-getting-cannot
+declare module '*&useResponsiveLoader=true' {
+  const defaultExport: {
+    srcSet: string;
+    images: {
+      height: number;
+      width: number;
+      path: string;
+    }[];
+    src: string;
+    placeholder: string;
+    toString(): string;
+  };
+  export default defaultExport;
+}
--- webpack/pluginsConfig.cjs
+++ webpack/pluginsConfig.cjs
@@ -7,48 +7,48 @@ module.exports = function ({ MiniCssExtractPlugin }) {
     new (require('html-webpack-plugin'))({ template: './src/index.html' }),
     new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
     new (require('eslint-webpack-plugin'))({ extensions: ['ts', 'tsx', 'js', 'jsx', 'json'] }),
-    new ImageMinimizerPlugin({
-      minimizer: {
-        // @see https://github.com/imagemin/imagemin
-        implementation: ImageMinimizerPlugin.imageminMinify,
-        options: {
-          // Lossless optimization with custom option
-          // Feel free to experiment with options for better result for you
-          // @see https://www.npmjs.com/search?q=keywords:imageminplugin
-          plugins: [
-            ['gifsicle', { interlaced: true }],
-            ['jpegtran', { progressive: true }],
-            ['optipng', { optimizationLevel: 5 }],
-            // Svgo configuration here https://github.com/svg/svgo#configuration
-            [
-              'svgo',
-              {
-                plugins: [
-                  {
-                    name: 'preset-default',
-                    params: {
-                      overrides: {
-                        removeViewBox: {
-                          active: false,
-                        },
-                        addAttributesToSVGElement: {
-                          params: {
-                            attributes: [
-                              {
-                                xmlns: 'http://www.w3.org/2000/svg',
-                              },
-                            ],
-                          },
-                        },
-                      },
-                    },
-                  },
-                ],
-              },
-            ],
-          ],
-        },
-      },
-    }),
+    // new ImageMinimizerPlugin({
+    //   minimizer: {
+    //     // @see https://github.com/imagemin/imagemin
+    //     implementation: ImageMinimizerPlugin.imageminMinify,
+    //     options: {
+    //       // Lossless optimization with custom option
+    //       // Feel free to experiment with options for better result for you
+    //       // @see https://www.npmjs.com/search?q=keywords:imageminplugin
+    //       plugins: [
+    //         ['gifsicle', { interlaced: true }],
+    //         ['jpegtran', { progressive: true }],
+    //         ['optipng', { optimizationLevel: 5 }],
+    //         // Svgo configuration here https://github.com/svg/svgo#configuration
+    //         [
+    //           'svgo',
+    //           {
+    //             plugins: [
+    //               {
+    //                 name: 'preset-default',
+    //                 params: {
+    //                   overrides: {
+    //                     removeViewBox: {
+    //                       active: false,
+    //                     },
+    //                     addAttributesToSVGElement: {
+    //                       params: {
+    //                         attributes: [
+    //                           {
+    //                             xmlns: 'http://www.w3.org/2000/svg',
+    //                           },
+    //                         ],
+    //                       },
+    //                     },
+    //                   },
+    //                 },
+    //               },
+    //             ],
+    //           },
+    //         ],
+    //       ],
+    //     },
+    //   },
+    // }),
   ].filter(Boolean);
 };

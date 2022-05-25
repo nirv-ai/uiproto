f864a90e3ec0d7a7b8b623a64854f85ea1bad432
noahehall <noahehall@users.noreply.github.com>
2022-05-25 13:12:13 -0700

    remove responsive loader and sharp and move on with life

--- webpack/moduleConfig.cjs
+++ webpack/moduleConfig.cjs
@@ -6,30 +6,32 @@
 // asset: automatically choose (e.g. url-loader with asset size limit)
 
 const imgTest = /\.(je?pg|a?png|gif|avif|webp)$/i;
+
+const filenameTemplate = '[file][query][contenthash][ext]';
 const getAssetLoaders = () => [
   {
     test: /\.(eot|otf|ttf|woff|woff2)$/,
     type: 'asset/resource',
-    generator: { filename: '[file][query]' },
+    generator: { filename: filenameTemplate },
   },
   {
     // @see https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types
     test: imgTest,
     type: 'asset/resource',
-    generator: { filename: '[file][query]' },
+    generator: { filename: filenameTemplate },
   },
   {
     test: /\.svg$/,
     type: 'asset/inline',
     generator: {
-      filename: '[file][query]',
+      filename: filenameTemplate,
       dataUrl: content => require('mini-svg-data-uri')(content.toString()),
     },
   },
   {
     test: /\.(mp4|webm)$/,
     type: 'asset/resource',
-    generator: { filename: '[file][query]' },
+    generator: { filename: filenameTemplate },
   },
 ];
 
@@ -37,22 +39,6 @@ module.exports = function ({ MiniCssExtractPlugin, isDev }) {
   return {
     rules: [
       ...getAssetLoaders(),
-      {
-        test: imgTest,
-        use: {
-          loader: 'responsive-loader',
-          options: {
-            name: 'images/[name]-[width]-[contenthash:7].[ext]',
-            adapter: require('responsive-loader/sharp'),
-            // sizes: [320, 640], // 960, 1200, 1800, 2400
-            placeholder: true,
-            placeholderSize: 20,
-            disable: false,
-            eModule: true,
-            emitFile: true,
-          },
-        },
-      },
       {
         test: /\.(csv|tsv)$/i,
         use: ['csv-loader'],

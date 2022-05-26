65b244925330ff19dc250bbfb02233c255fef8ca
noahehall <noahehall@users.noreply.github.com>
2022-05-26 13:09:22 -0700

    fix bgimage background logic for rendering multiple bg images

--- src/Library/Image/BgImg.tsx
+++ src/Library/Image/BgImg.tsx
@@ -2,21 +2,28 @@ import { type FC } from 'react';
 
 import { Text, getClass, type TextInterface } from 'src/Library';
 
+export const parseSrcToBgImgCssProp = (src: string): string => {
+  const [url, ...rest] = src.trim().split(' ');
+  return `url(${url}) ${rest.join(' ')}`.trim();
+};
+
 export interface BgImgInterface extends TextInterface {
   [x: string]: any;
   alt?: string;
   block?: boolean;
   className?: string;
   gradient?: boolean;
-  height: string;
+  height?: string;
   src: string | string[];
   title?: string;
-  width: string;
+  width?: string;
 }
 
 /**
+ * TODO:(noah) need to add bg gradient examples & logic
  * @see https://www.davidmacd.com/blog/alternate-text-for-css-background-images.html
  * @see https://www.davidmacd.com/blog/what-is-pure-decoration-alt-text-in-wcag.html
+ * @see https://stackoverflow.com/questions/24682193/css-background-shorthand-property-not-working-with-background-size
  */
 export const BgImg: FC<BgImgInterface> = ({
   alt,
@@ -35,12 +42,21 @@ export const BgImg: FC<BgImgInterface> = ({
     throw new Error('You must provide an alt for all bg images that arent gradients');
   }
 
+  if (!block && (!width || !height)) {
+    console.error('\n\n props before error', props);
+    throw new Error('You must provide a width and height for all bg images that arent block');
+  }
+
   const useClass = getClass(className, 'bg-img', block ? 'block' : 'inline');
+
+  const background = Array.isArray(src)
+    ? src.map(src => parseSrcToBgImgCssProp(src)).join(', ')
+    : parseSrcToBgImgCssProp(src);
 
   const bgImgCss = {
     width,
     height,
-    backgroundImage: Array.isArray(src) ? src.map(src => `url(${src})`).join(', ') : `url(${src})`,
+    background,
   };
 
   return (
--- src/Screens/Images/Examples/BgImgExamples.tsx
+++ src/Screens/Images/Examples/BgImgExamples.tsx
@@ -2,6 +2,7 @@ import { type FC } from 'react';
 
 import { CopyPastas, P, BgImg, type PastaItemInterface } from 'src/Library';
 import computerImgJpg from 'src/assets/images/nicJackson/computer.jpg';
+import photogapherImgJpg from 'src/assets/images/nicJackson/photographer.jpg';
 
 const bgImgItem: PastaItemInterface = (El, about, props = {}) => ({
   PastaEl: <El {...props} />,
@@ -18,7 +19,20 @@ const imgProps = {
 const BgImgCopyPastaConfig = [
   bgImgItem(BgImg, 'BgImg Element', {
     ...imgProps,
-    src: computerImgJpg,
+    src: `${computerImgJpg} no-repeat 0 0 /contain`,
+    alt: 'ima span with a background-image',
+  }),
+  bgImgItem(BgImg, 'BgImg Element', {
+    ...imgProps,
+    width: '100%',
+    src: `${computerImgJpg} center center /cover no-repeat `,
+    alt: 'ima span with a background-image',
+  }),
+  bgImgItem(BgImg, 'BgImg Element', {
+    ...imgProps,
+    width: '100%',
+    block: true,
+    src: [`${computerImgJpg} no-repeat 0 0 /150px`, `${photogapherImgJpg} no-repeat 100% 0 /150px`],
     alt: 'ima span with a background-image',
   }),
 ];
